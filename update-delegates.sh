2792a86bf79ee145279ca6572351273d93c5a6a9
Xavier Fernandez <xavier@crservers.com>
2022-06-15 22:40:51 -0500

    fix(multisig): format code

--- update-delegates.sh
+++ update-delegates.sh
@@ -4,7 +4,7 @@
 API="https://eos.greymass.com"
 
 # Get user input
-echo "You may need to set the delegates accounts first - check the contents of `current-eden-delegates.json` and `new-delegates.json` files."
+echo "You may need to set the delegates accounts first - check the contents of current-eden-delegates.json and new-delegates.json files."
 echo ""
 read -p "Enter the name and permission of the proposing account (e.g. account@active):" PROPOSER
 read -p "Enter the name of the proposal: " PROPOSAL
