53bc41632fbdbee0ea76b2611bb300844f34c2bd
Xavier Fernandez <xavier@crservers.com>
2022-06-15 22:43:37 -0500

    fix(multisig): fix token transfer command

--- token-transfer.sh
+++ token-transfer.sh
@@ -1,3 +1,6 @@
 #/bin/sh
 
-cleos -u https://eos.greymass.com transfer eosio.token transfer eosio.grants genesis.eden "10.0000 EOS" "Hodl!" -p eosio@active
+# Set EOS Mainnet API to your favorite node.
+API="https://eos.greymass.com"
+
+cleos -u $API transfer eosio.grants genesis.eden "10.0000 EOS" "Hodl!" -p eosio@active
